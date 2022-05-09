4f7f8c9e8b03e1fdf57803d37c42ae0b508f80ae
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-05-09 17:07:33 -0500

    Adding ExplorerService.js file and its test, also filterByMission method

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,16 @@
+const ExplorerService = require("./../../lib/services/ExplorerService");
+const Reader = require("./../../lib/utils/Reader");
+const explorers = Reader.readerJsonFile("./explorers.json");
+
+describe("Tests for ExplorerService", () => {
+  test("Filter explorers By Mission", () => {
+    const explorersbymission = ExplorerService.filterByMission(explorers, 'node')
+    expect(explorersbymission[0]).toMatchObject({
+      name: expect.any(String),
+      githubUsername: expect.any(String),
+      score: expect.any(Number),
+      mission: expect.stringMatching(/node/),
+      stacks: expect.any(Array)
+    })
+  });
+});
