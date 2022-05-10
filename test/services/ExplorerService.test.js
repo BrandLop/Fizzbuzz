c98bb2ab876afbf783c31747aca151fc7108d756
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-05-09 21:18:36 -0500

    Setting up style-guide with eslint

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -3,25 +3,25 @@ const Reader = require("./../../lib/utils/Reader");
 const explorers = Reader.readerJsonFile("./explorers.json");
 
 describe("Tests for ExplorerService", () => {
-  test("Filter explorers By Mission", () => {
-    const explorersbymission = ExplorerService.filterByMission(explorers, 'node')
-    expect(explorersbymission[0]).toMatchObject({
-      name: expect.any(String),
-      githubUsername: expect.any(String),
-      score: expect.any(Number),
-      mission: expect.stringMatching(/node/),
-      stacks: expect.any(Array)
-    })
-  });
+    test("Filter explorers By Mission", () => {
+        const explorersbymission = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersbymission[0]).toMatchObject({
+            name: expect.any(String),
+            githubUsername: expect.any(String),
+            score: expect.any(Number),
+            mission: expect.stringMatching(/node/),
+            stacks: expect.any(Array)
+        });
+    });
 
-  test("Amount of explorers by mission", () => {
-    const amountExplorersinNode = ExplorerService.getAmountOfExplorersByMission(explorers, 'node')
-    expect(amountExplorersinNode).toBe(10)
-  });
-  
-  test("Explorers usernames by mission", () => {
-    const explorersusernamesbymission = ExplorerService.getExplorersUsernamesByMission(explorers, 'node')
-    const expected = ['ajolonauta1','ajolonauta2','ajolonauta3','ajolonauta4','ajolonauta5','ajolonauta11','ajolonauta12','ajolonauta13','ajolonauta14','ajolonauta15']
-    expect(explorersusernamesbymission).toEqual(expect.arrayContaining(expected))
-  });
+    test("Amount of explorers by mission", () => {
+        const amountExplorersinNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        expect(amountExplorersinNode).toBe(10);
+    });
+
+    test("Explorers usernames by mission", () => {
+        const explorersusernamesbymission = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        const expected = ["ajolonauta1","ajolonauta2","ajolonauta3","ajolonauta4","ajolonauta5","ajolonauta11","ajolonauta12","ajolonauta13","ajolonauta14","ajolonauta15"];
+        expect(explorersusernamesbymission).toEqual(expect.arrayContaining(expected));
+    });
 });
--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,28 +1,27 @@
 const FizzbuzzService = require("./../../lib/services/FizzbuzzService");
 
 describe("Tests for FizzbuzzService class", () => {
-  test("Apply FIZZBUZZ validation", () => {
-    const explorer15 = {name: "Explorer15", score: 15}
-    FizzbuzzService.applyValidationInExplorer(explorer15)
-    expect(explorer15).toMatchObject({name: "Explorer15", score: 15, trick: "FIZZBUZZ"})
-  })
+    test("Apply FIZZBUZZ validation", () => {
+        const explorer15 = {name: "Explorer15", score: 15};
+        FizzbuzzService.applyValidationInExplorer(explorer15);
+        expect(explorer15).toMatchObject({name: "Explorer15", score: 15, trick: "FIZZBUZZ"});
+    });
 
-  test("Apply FIZZ validation", () => {
-    const explorer3 = {name: "Explorer3", score: 3}
-    FizzbuzzService.applyValidationInExplorer(explorer3)
-    expect(explorer3).toMatchObject({name: "Explorer3", score: 3, trick: "FIZZ"})
-  })
+    test("Apply FIZZ validation", () => {
+        const explorer3 = {name: "Explorer3", score: 3};
+        FizzbuzzService.applyValidationInExplorer(explorer3);
+        expect(explorer3).toMatchObject({name: "Explorer3", score: 3, trick: "FIZZ"});
+    });
 
-  test("Apply BUZZ validation", () => {
-    const explorer5 = {name: "Explorer5", score: 5}
-    FizzbuzzService.applyValidationInExplorer(explorer5)
-    expect(explorer5).toMatchObject({name: "Explorer5", score: 5, trick: "BUZZ"})
-  })
+    test("Apply BUZZ validation", () => {
+        const explorer5 = {name: "Explorer5", score: 5};
+        FizzbuzzService.applyValidationInExplorer(explorer5);
+        expect(explorer5).toMatchObject({name: "Explorer5", score: 5, trick: "BUZZ"});
+    });
 
-  test("Apply number validation", () => {
-    const explorer1 = {name: "Explorer1", score: 1}
-    FizzbuzzService.applyValidationInExplorer(explorer1)
-    expect(explorer1).toMatchObject({name: "Explorer1", score: expect.any(Number), trick: expect.any(Number)})
-  })
-  
-})
+    test("Apply number validation", () => {
+        const explorer1 = {name: "Explorer1", score: 1};
+        FizzbuzzService.applyValidationInExplorer(explorer1);
+        expect(explorer1).toMatchObject({name: "Explorer1", score: expect.any(Number), trick: expect.any(Number)});
+    });
+});
