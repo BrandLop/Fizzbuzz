5ff05832e922234cfe3785233f205113afb6381f
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-05-09 19:03:00 -0500

    Adding BUZZ validation and its test

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -8,9 +8,15 @@ describe("Tests for FizzbuzzService class", () => {
   })
 
   test("Apply FIZZ validation", () => {
-    const explorer3 = {name: "Explorer3", score: 3, trick: "FIZZ"}
+    const explorer3 = {name: "Explorer3", score: 3}
     FizzbuzzService.applyValidationInExplorer(explorer3)
     expect(explorer3).toMatchObject({name: "Explorer3", score: 3, trick: "FIZZ"})
+  })
+
+  test("Apply BUZZ validation", () => {
+    const explorer5 = {name: "Explorer5", score: 5}
+    FizzbuzzService.applyValidationInExplorer(explorer5)
+    expect(explorer5).toMatchObject({name: "Explorer5", score: 5, trick: "BUZZ"})
   })
   
 })
