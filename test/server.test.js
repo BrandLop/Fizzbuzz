eb6c26d05e130edd97ac579f457921a5737b2d56
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-05-13 23:16:26 -0500

    Endpoint that retunrs an object with mission name and its explorers list

--- test/server.test.js
+++ test/server.test.js
@@ -144,6 +144,22 @@ describe("GET taks", () => {
         expect(response.body).toMatchObject({mission: expect.stringMatching(/java/), quantity: 5});
     });
 
+    test("Should return an object with an usernames list of the node mision", async () => {
+        const expected = ["ajolonauta1","ajolonauta2","ajolonauta3","ajolonauta4","ajolonauta5","ajolonauta11","ajolonauta12","ajolonauta13","ajolonauta14","ajolonauta15"];
+        const response = await request(app)
+            .get("/v1/explorers/usernames/node");
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toMatchObject({mission: expect.stringMatching(/node/), explorers: expect.arrayContaining(expected)});
+    });
+
+    test("Should return an object with an usernames list of the java mision", async () => {
+        const expected = ["ajolonauta6","ajolonauta7","ajolonauta8","ajolonauta9","ajolonauta10"];
+        const response = await request(app)
+            .get("/v1/explorers/usernames/java");
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toMatchObject({mission: expect.stringMatching(/java/), explorers: expect.arrayContaining(expected)});
+    });
+
     afterAll(() => {
         server.close();
     });
